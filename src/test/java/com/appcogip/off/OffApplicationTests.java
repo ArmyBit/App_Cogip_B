d6d00b2743ddd5a1eee7a9b143722882b9519ab9
Saad Lemzaouak <saad.lemzaouak@edu.isga.ma>
2024-05-11 03:15:22 +0100

    First Commit

---
+++ src/test/java/com/appcogip/off/OffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appcogip.off;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
